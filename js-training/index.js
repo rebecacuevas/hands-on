ea9b6a7e3bab8738e0e851b853ff09bd8947f635
Rebeca Cuevas <rebecaacuevasa@gmail.com>
2022-01-09 23:43:01 -0600

    weekend

---
+++ js-training/index.js
@@ -0,0 +1,37 @@
+// document.getElementById("count").innerText = 5;
+
+// initialize the count at 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count in the HTML to reflect the new count
+
+function increment() {
+    console.log("The button was clicked");
+}
+
+function number() {
+    const myNumber = 42;
+    console.log(myNumber);
+}
+
+number();
+
+function raceTime(){
+    let lap1 = 34;
+    let lap2 = 33;
+    let lap3 = 36;
+
+    total = lap1 + lap2 + lap3;
+    console.log(total);
+}
+
+raceTime(); 
+
+
+
+
+
+
+
+
+
